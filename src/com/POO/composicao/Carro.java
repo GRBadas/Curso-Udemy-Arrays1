1c7df152c9163f82ae2d9af29680553f0b191a01
Gabriel Badaró <grbadas@gmail.com>
2022-05-11 17:15:51 -0300

    simple-poo

---
+++ src/com/POO/composicao/Carro.java
@@ -0,0 +1,28 @@
+package com.POO.composicao;
+
+public class Carro {
+
+    Motor motor = new Motor();
+
+    public void acelerar(){
+        if(motor.fatorInjecao < 2.6 )
+        motor.fatorInjecao += 0.4;
+    }
+
+    void frear(){
+        if(motor.fatorInjecao > 0.5)
+        motor.fatorInjecao -= 0.4; 
+    }
+    
+    public void ligar(){
+        motor.ligado = true;
+    }
+
+    void desligar(){
+        motor.ligado = false;
+    }
+
+    boolean estaLigado(){
+        return motor.ligado;
+    }
+}
